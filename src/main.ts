c2b1a91956179ddc9483352d4cc7d1b5ecea3bf3
Brian Gershon <briangershon@users.noreply.github.com>
2023-06-27 20:03:07 -0700

    Add p5.js and initial sketch

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,16 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+import * as p5 from "p5";
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+export const sketch = (p: p5) => {
+  p.setup = () => {
+    p.createCanvas(400, 400);
+  };
+
+  p.draw = () => {
+    p.background(220);
+    p.ellipse(50, 50, 80, 80);
+  };
+};
+
+export const myp5 = new p5(sketch, document.body);
